616906745a6ebc5631ab666c9c3212138df4619f
Zuzah <murtaza.hasni@gmail.com>
2015-09-30 20:41:27 -0400

    initial commit with skeleton code

---
+++ best.R
@@ -0,0 +1,11 @@
+#Write a function called best that take two arguments: the 2-character
+#abbreviated name of a state and an outcome name. The function reads
+#the outcome-of-care-measures.csv ﬁle and returns a character vector
+#with the name of the hospital that has the best (i.e. lowest)
+#30-day mortality for the speciﬁed outcome in that state
+
+
+best <- function(state, outcome) { ## Read outcome data
+## Check that state and outcome are valid
+## Return hospital name in that state with lowest 30-day death ## rate
+}
